99cd87570fd0e29c0946e49abb6f5b33959bc787
Nicola Pietroluongo <nik.longstone@gmail.com>
2015-10-20 18:42:56 +0100

    update

---
+++ class/refuseBadRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+interface Animal
+{
+    public function fly()
+    public function swim()
+    public function eat()
+}
+
+class Fish extends Animal {
+
+    public function swim()
+    public function fly()
+    {
+        //trow Exception
+    }
+}
+
+interface Animal
+{
+    public function eat()
+}
+interface SeaAnimal implements Animal
+{
+    public function swim()
+}
+class Fish implements SeaAnimal
