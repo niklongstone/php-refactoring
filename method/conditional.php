f624b9741a22c7679be2d6c31e1fcb1dd737d6b9
Nicola Pietroluongo <nik.longstone@gmail.com>
2015-10-25 17:29:03 +0000

    Update conditional.php

--- method/conditional.php
+++ method/conditional.php
@@ -6,7 +6,7 @@ if ($currentDate < $christmasDate && $currentDate > $summerDate) {
     $saleDiscount = ($yearBasicRate + $normalDiscount) * $quantity;
 }
 
-$saleDiscount = getNormalDiscount(quantity);
+$saleDiscount = getNormalDiscount($quantity);
 if (isChristmas($currentDate)) {
-    $saleDiscount = getCristhmasDiscount(quantity);
+    $saleDiscount = getCristhmasDiscount($quantity);
 }
